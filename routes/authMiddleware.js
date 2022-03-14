30137cb14e5bc1b2d576a7cca39af71107eef918
chris okwakol <chrisokwakol@gmail.com>
2022-03-14 08:17:12 +0300

    added cors url

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -14,3 +14,4 @@ module.exports.isAuth = (req, res, next) => {
 //         res.status(401).json({ msg: 'You are not authorized to view this resource because you are not an admin.' });
 //     }
 // }
+// 
